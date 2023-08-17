c1664593af5f342b4bb4c4e31b49036021f2c612
Lirancj <liran.chaimjan123@gmail.com>
2023-08-17 23:33:01 +0300

    Fix

--- tests/tester.py
+++ tests/tester.py
@@ -2,11 +2,13 @@ import subprocess
 import os
 from pathlib import Path
 
-example_files = os.listdir('tests/examples')
+example_files = map(lambda path: Path(path), os.listdir('tests/examples'))
+
 
 for file in example_files:
+    absolute_file = file.resolve()
     print(f"Running tests for {file}")
-    process = subprocess.run(["python", "vulnerabilities_scanner.py", file])
+    process = subprocess.run(["python", "vulnerabilities_scanner.py", absolute_file])
     exit_code = process.returncode
     print(f"Exit code for {file}: {exit_code}")
 
