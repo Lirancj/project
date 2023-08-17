168bc34157d5b3d118b6f9972b3472360d1653c4
Lirancj <liran.chaimjan123@gmail.com>
2023-08-17 23:25:04 +0300

    Add tests

---
+++ tests/tester.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+example_files = os.listdir('examples')
+
+for file in example_files:
+    print(f"Running tests for {file}")
+    process = subprocess.run(["python", "/vulnerabilities_scanner.py", file])
+    exit_code = process.returncode
+    print(f"Exit code for {file}: {exit_code}")
+
+    if exit_code != 1:
+        print(f"Vulnerability was not detected in {file}")
+        exit(1)
